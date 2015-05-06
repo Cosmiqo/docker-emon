e935985ba4473ecc8f88e64c3a025806fc2779e0
Lam Kee Wei <keewei.lam@gmail.com>
2015-05-06 13:10:58 -0400

    Made supplying a password mandatory as it will make data migration easier

--- emoncms/db.sh
+++ emoncms/db.sh
@@ -11,11 +11,6 @@ while [[ RET -ne 0 ]]; do
   RET=$?
 done
 
-# Generate a password using pwgen if MYSQL_PASSWORD is not passed in
-if [[ -z $MYSQL_PASSWORD ]]; then 
-  MYSQL_PASSWORD="$(pwgen -s 12 1)"
-fi
-
 # Initialize the db and create the user 
 echo "CREATE DATABASE emoncms;" >> init.sql
 echo "CREATE USER 'emoncms'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';" >> init.sql
@@ -23,19 +18,16 @@ echo "GRANT ALL ON emoncms.* TO 'emoncms'@'localhost';" >> init.sql
 echo "flush privileges;" >> init.sql
 mysql < init.sql
 
-# Update the settings file for emoncms
-EMON_DIR="/var/www/html"
-cp "$EMON_DIR/default.settings.php" "$EMON_DIR/settings.php"
-sed -i "s/_DB_USER_/emoncms/" "$EMON_DIR/settings.php"
-sed -i "s/_DB_PASSWORD_/$MYSQL_PASSWORD/" "$EMON_DIR/settings.php"
-
-echo "==========================================================="
-echo "The username and password for the emoncms user is:"
-echo ""
-echo "   username: emoncms"
-echo "   password: $MYSQL_PASSWORD"
-echo ""
-echo "==========================================================="
-
 # Cleanup
 rm init.sql
+
+# Stop MySQL Server
+service mysql stop > /dev/null 2>&1
+
+RET=0
+while [[ RET -eq 0 ]]; do
+  echo "Waiting for MySQL to stop..."
+  sleep 5
+  mysql -e "status" > /dev/null 2>&1
+  RET=$?
+done
