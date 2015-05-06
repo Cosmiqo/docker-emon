51e104ffc68a1e8cfa5be32b6143e6e0023ff133
Lam Kee Wei <keewei.lam@gmail.com>
2015-05-06 03:40:32 -0400

    Added script to backup mysql folders in running container

---
+++ backup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+if [[ "$#" -ne 1 ]]; then
+  echo "USAGE: ./backup.sh CONTAINER NAME"
+  exit 1
+fi
+
+docker run --rm --volumes-from "$1" -v $(pwd):/backup ubuntu tar cvf /backup/backup.tar /var/lib/mysql/emoncms \
+  /var/lib/phpfiwa /var/lib/phpfina /var/lib/phptimeseries /var/lib/timestore
