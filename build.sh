76dd908c4af32721ac8fbd7a4980185df62ab164
Lam Kee Wei <keewei.lam@gmail.com>
2015-05-07 12:04:49 -0400

    Updated build script

--- build.sh
+++ build.sh
@@ -1,22 +1,20 @@
 #!/bin/bash 
 
-EMONBASE_DIR="emonbase"
-EMONCMS_DIR="emoncms"
-EMONDATA_DIR="emondata"
+EMONBASE_DIR="images/emonbase"
+EMONCMS_DIR="images/emoncms"
+EMONDATA_DIR="images/emondata"
 
 # Build the base image
 cd "$EMONBASE_DIR"
 docker build -t cosmiqo/emonbase .
-cd ..
+cd ../..
 
 # Build the data image
 cd "$EMONDATA_DIR"
 docker build -t cosmiqo/emondata .
-cd ..
+cd ../..
 
 # Build the emoncms image
 cd "$EMONCMS_DIR"
 docker build -t cosmiqo/emoncms .
-cd ..
-
-docker pull busybox
+cd ../..
